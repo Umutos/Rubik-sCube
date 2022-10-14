14e4d5b58285206db72e2f73b8c1001e35463cf3
Antoine MORDANT <a.mordant@student.isartdigital.com>
2022-10-14 11:42:56 +0200

    add cube prefab and rubikscube generator

---
+++ Assets/Rotation/RubiksCubeCreator.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubiksCubeCreator : MonoBehaviour
+{
+    [SerializeField] GameObject cube;
+    [SerializeField] GameObject center;
+    List<GameObject> rubiksCube;
+    public int sideLength = 3;
+    int currentLength;
+    Quaternion rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = Quaternion.identity;
+        rubiksCube = new List<GameObject>();
+        if (sideLength > 10)
+            sideLength = 10;
+        if (sideLength < 2)
+            sideLength = 2;
+        currentLength = sideLength;
+        Generate(currentLength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentLength != sideLength)
+        {
+            if (sideLength > 10)
+                sideLength = 10;
+            if (sideLength < 2)
+                sideLength = 2;
+
+            currentLength = sideLength;
+            DeleteCube();
+            Generate(currentLength);
+        }
+    }
+
+    void Generate(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                for (int k = 0; k < length; k++)
+                {
+                    GameObject c = Instantiate(cube, new Vector3(i * 2 - length / 2, j * 2 - length / 2, k * 2 - length / 2), rotation);
+                    //c.transform.localScale = new Vector3(3f / length, 3f / length, 3f / length);
+                    c.transform.parent = center.transform;
+                    rubiksCube.Add(c);
+                }
+            }
+        }
+    }
+
+    void DeleteCube()
+    {
+        for (int i = 0; i < rubiksCube.Count; i++)
+            Destroy(rubiksCube[i]);
+
+        rubiksCube.Clear();
+    }
+}
